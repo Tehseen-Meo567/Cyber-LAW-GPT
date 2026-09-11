fe60403cee17bbf5f5c69e5ad254eceee84578c8
Tehseen Meo <tehseenmeo7@gmail.com>
2026-09-11 20:59:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -209,7 +209,8 @@ def call_groq(system_prompt: str, question: str, api_key: str) -> str:
 
     client = Groq(api_key=api_key)
     completion = client.chat.completions.create(
-        model="llama-3.3-70b-versatile", "llama-3.1-8b-instant",
+        model= ["llama-3.3-70b-versatile", 
+                "llama-3.1-8b-instant" ],
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": question},
